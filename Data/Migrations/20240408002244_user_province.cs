36441144181dd1531c440d675f64617d24298e7e
GoriBashiru <158530661+GoriBashiru@users.noreply.github.com>
2024-04-07 21:13:56 -0400

    Add files via upload

---
+++ Data/Migrations/20240408002244_user_province.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContactManager.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class user_province : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Zip",
+                table: "Contact",
+                newName: "Province");
+
+            migrationBuilder.RenameColumn(
+                name: "State",
+                table: "Contact",
+                newName: "Postcode");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Province",
+                table: "Contact",
+                newName: "Zip");
+
+            migrationBuilder.RenameColumn(
+                name: "Postcode",
+                table: "Contact",
+                newName: "State");
+        }
+    }
+}
---
+++ Model/Contact.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactManager.Models
+{
+    public class Contact
+    {
+        public int ContactId { get; set; }
+
+        // user ID from AspNetUser table.
+        public string? OwnerID { get; set; }
+
+
+        public string? Name { get; set; }
+        public string? Address { get; set; }
+        public string? City { get; set; }
+        public string? Province { get; set; }
+        public string? Postcode { get; set; }
+        [DataType(DataType.EmailAddress)]
+        public string? Email { get; set; }
+
+        public ContactStatus Status { get; set; }
+    }
+
+    public enum ContactStatus
+    {
+        Submitted,
+        Approved,
+        Rejected
+    }
+
+}
+
+
+
